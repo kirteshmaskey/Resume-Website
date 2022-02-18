fba456f15a683f6d0d9208b954f97c2c361e42d1
Kirtesh Maskey <kirteshmaske123@gmail.com>
2022-02-18 21:46:19 +0530

    Adjust alignment

--- main.js
+++ main.js
@@ -11,9 +11,9 @@ function dropDownMenu(){
       else{
         dm.className = "nav-list"
         document.getElementById('info-content').style.paddingTop = "20%";
-        document.getElementById('About-me').style.paddingTop = "20%";
-        document.getElementById('Resume').style.paddingTop = "20%";
-        document.getElementById('Certificate').style.paddingTop = "20%";
-        document.getElementById('Contact-me').style.paddingTop = "20%";
+        document.getElementById('About-me').style.paddingTop = "40%";
+        document.getElementById('Resume').style.paddingTop = "40%";
+        document.getElementById('Certificate').style.paddingTop = "32%";
+        document.getElementById('Contact-me').style.paddingTop = "40%";
     }
 }
