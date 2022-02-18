add5b0c9c9126c152d44dd394836621ac39bb391
Kirtesh Maskey <kirteshmaske123@gmail.com>
2022-02-18 15:41:05 +0530

    Updated Certificate and Contact me section

---
+++ display.js
@@ -0,0 +1,35 @@
+function displayHome(){
+    document.getElementById('info-content').style.display = 'block';
+    document.getElementById('About-me').style.display = 'none';
+    document.getElementById('Resume').style.display = 'none';
+    document.getElementById('Certificate').style.display = 'none';
+    document.getElementById('Contact-me').style.display = 'none';
+}
+function displayAboutMe(){
+    document.getElementById('info-content').style.display = 'none';
+    document.getElementById('About-me').style.display = 'block';
+    document.getElementById('Resume').style.display = 'none';
+    document.getElementById('Certificate').style.display = 'none';
+    document.getElementById('Contact-me').style.display = 'none';
+}
+function displayResume(){
+    document.getElementById('info-content').style.display = 'none';
+    document.getElementById('About-me').style.display = 'none';
+    document.getElementById('Resume').style.display = 'block';
+    document.getElementById('Certificate').style.display = 'none';
+    document.getElementById('Contact-me').style.display = 'none';
+}
+function displayCertificate(){
+    document.getElementById('info-content').style.display = 'none';
+    document.getElementById('About-me').style.display = 'none';
+    document.getElementById('Resume').style.display = 'none';
+    document.getElementById('Certificate').style.display = 'block';
+    document.getElementById('Contact-me').style.display = 'none';
+}
+function displayContactMe(){
+    document.getElementById('info-content').style.display = 'none';
+    document.getElementById('About-me').style.display = 'none';
+    document.getElementById('Resume').style.display = 'none';
+    document.getElementById('Certificate').style.display = 'none';
+    document.getElementById('Contact-me').style.display = 'block';
+}
--- main.js
+++ main.js
@@ -3,19 +3,17 @@ function dropDownMenu(){
     if(dm.className == "nav-list"){
         dm.className += " responsive";
         document.getElementById('info-content').style.paddingTop = "45vh";
+        document.getElementById('About-me').style.paddingTop = "45vh";
+        document.getElementById('Resume').style.paddingTop = "45vh";
+        document.getElementById('Certificate').style.paddingTop = "45vh";
+        document.getElementById('Contact-me').style.paddingTop = "45vh";
       }
       else{
         dm.className = "nav-list"
         document.getElementById('info-content').style.paddingTop = "20%";
+        document.getElementById('About-me').style.paddingTop = "20%";
+        document.getElementById('Resume').style.paddingTop = "20%";
+        document.getElementById('Certificate').style.paddingTop = "20%";
+        document.getElementById('Contact-me').style.paddingTop = "20%";
     }
 }
-
-
-/*
-var txt = 'Lorem ipsum dummy text blabla.';
-var speed = 50;
-  if (i < txt.length) {
-    document.getElementById("info-content").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }*/
